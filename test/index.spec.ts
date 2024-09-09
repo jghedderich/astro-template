7b25080b741d5b717ed32341ba130fd9b95f0c31
Juan Hedderich <hedderich.dev@gmail.com>
2024-09-09 15:10:12 -0400

    feat(ui): :lipstick: add navbar and footer

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,15 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4321');
+});
 
-  // Expect a title "to contain" a substring.
+test('has title', async ({ page }) => {
   await expect(page).toHaveTitle('Astro Template');
 });
 
 test('use the form', async ({ page }) => {
-  await page.goto('http://localhost:4321');
-
   await page.getByLabel('Username').fill('testing-username');
   await page.getByLabel('Email').fill('testing@gmail.com');
   await page.getByLabel('Password').fill('testing-password');
